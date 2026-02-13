d64e231d69fc18ee7a953b825ffddf79814ebd69
Taavi Ansper <taavi.ansper@cyber.ee>
2026-02-13 14:21:59 +0200

    Add new gitlabGroupVisibility variable

--- src/LdapSyncCommand.php
+++ src/LdapSyncCommand.php
@@ -817,6 +817,10 @@ class LdapSyncCommand extends Command
                     $addProblem("warning", "gitlab->options->projectCreationLevel missing. (Assuming 'developer')");
                     $config["gitlab"]["options"]["projectCreationLevel"] = 'developer';
                 }
+                if (!isset($config["gitlab"]["options"]["gitlabGroupVisibility"])) {
+                    $addProblem("warning", "gitlab->options->gitlabGroupVisibility missing. (Assuming 'private')");
+                    $config["gitlab"]["options"]["gitlabGroupVisibility"] = 'private';
+                }
             }
             // >> Gitlab options
 
@@ -1819,7 +1823,7 @@ class LdapSyncCommand extends Command
                     $ldapRootGroupId = $gitlab->groups()->show($ldapRootGroup);
                 }
                 // TODO https://github.com/GitLabPHP/Client/blob/12.0/src/Api/Groups.php#L75 I don't want to add all of the other default options. I want to do something like this. create($gitlabGroupName, $gitlabGroupPath, "parent_id" = $ldapRootGroupId) ,but with the current code this doesn't work and I have to add all of the previous arguments aswell as defaults.
-                !$this->dryRun ? ($gitlabGroup = $gitlab->groups()->create($gitlabGroupName, $gitlabGroupPath, null, "private", null, null, $ldapRootGroupId)) : $this->logger?->warning("Operation skipped due to dry run.");
+                !$this->dryRun ? ($gitlabGroup = $gitlab->groups()->create($gitlabGroupName, $gitlabGroupPath, null, $gitlabGroupVisibility, null, null, $ldapRootGroupId)) : $this->logger?->warning("Operation skipped due to dry run.");
             }
 
             $gitlabGroupId = (is_array($gitlabGroup) && isset($gitlabGroup["id"]) && is_int($gitlabGroup["id"])) ? $gitlabGroup["id"] : sprintf("dry:%s", $gitlabGroupPath);
@@ -1921,7 +1925,8 @@ class LdapSyncCommand extends Command
             $ldapGroupMembers = $ldapGroupsSafe[$gitlabGroupName];
 
             !$this->dryRun ? ($gitlabGroup = $gitlab->groups()->update($gitlabGroupId, [
-              "project_creation_level" => $config["gitlab"]["options"]["projectCreationLevel"]
+                "project_creation_level" => $config["gitlab"]["options"]["projectCreationLevel"],
+                "visibility" => $config["gitlab"]["options"]["gitlabGroupVisibility"]
             ])) : $this->logger->warning("Operation skipped due to dry run.");
 
             /** @var GitlabGroupArray|null $gitlabUser */
